6edb23a65b30ea985a74eeb316ce1323ae6e4a54
Wiremu Casey <s3661841@student.rmit.edu.au>
2024-09-18 18:50:43 +1000

    Refactor Prototype.py and add Chatbot feature
    - Refactor Prototype.py to improve code structure and readability
    - Add Chatbot feature to allow users to interact with the bot

--- Prototype.py
+++ Prototype.py
@@ -3,91 +3,48 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-#TODO: Turn this into a chatbot feature: 
-#Try: https://github.com/AI-Yash/st-chat 
-#or look through https://streamlit.io/components?category=llms 
-#or https://docs.streamlit.io/develop/tutorials/llms/llm-quickstart
-
-st.chat_input("Say something")
+from streamlit_option_menu import option_menu
+from src.chat import Chatbot
 
 # Title
 st.title('Prototype')
 
-
-# Header
-st.header('Data Tables')
-
-#TODO: Turn the data into classes where the tabs will be for displaying data
-st.sidebar.radio('Choose:',[1,2])
-
-# Subheader
-st.subheader('Rain Stations Data')
-
 # Load rain_stations_df
 rain_stations_df = './Data/rain_stations_df.csv'
-
 rain_stations_data = pd.read_csv(rain_stations_df)
 
+# Prepare data for map visualization
+rain_scatter = rain_stations_data[['Longitude', 'Latitude', 'Elevation', 'Station Name']]
+rain_scatter.columns = ['Longitude', 'Latitude', 'Elevation', 'Station_Name']
 
-# Display data
-'''
-st.table(rain_stations_data)
+# Sidebar with tabs
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Select Data",
+        options=["Rain Stations Data", "Table 2 Data", "Table 3 Data"],
+        icons=["cloud-rain", "table", "table"],
+        menu_icon="cast",
+        default_index=0,
+    )
 
-'''
+# Display selected data
+if selected == "Rain Stations Data":
+    st.subheader('Rain Stations Data')
+    st.scatter_chart(rain_scatter, x='Longitude', y='Latitude', size='Elevation', color='Station_Name')
+elif selected == "Table 2 Data":
+    st.subheader('Table 2 Data')
+    Table_2_table_df = './Data/Table 2_table.csv'
+    Table_2_table_data = pd.read_csv(Table_2_table_df)
+    st.table(Table_2_table_data)
+elif selected == "Table 3 Data":
+    st.subheader('Table 3 Data')
+    Table_3_table_df = './Data/Table 3_table.csv'
+    Table_3_table_data = pd.read_csv(Table_3_table_df)
+    st.table(Table_3_table_data)
 
-# Scatter plot
-# Prepare data for scatter chart
-# This code snippet is preparing data for a scatter chart visualization using Streamlit in Python.
-# Here's a breakdown of what each part of the code is doing:
-scatter_data = rain_stations_data[['Longitude', 'Latitude', 'Elevation', 'Station Name']]
-scatter_data.columns = ['Longitude', 'Latitude', 'Elevation', 'Station_Name']
+# Chatbot feature
+st.subheader('Chat with the Bot')
+chatbot = Chatbot()
 
-# Create scatter chart
-
-st.scatter_chart(scatter_data, x='Longitude', y='Latitude', size='Elevation', color='Station_Name')
-
-'''
-    
-# Subheader
-st.subheader('Table 2 Data')
-
-'''
-
-# Load Table 2_table
-'''
-Table_2_table_df = './Data/Table 2_table.csv'
-Table_2_table_data = pd.read_csv(Table_2_table_df)
-
-'''
-
-# Display data
-
-
-'''
-st.table(Table_2_table_data)
-
-'''
-
-# Subheader
-'''
-
-st.subheader('Table 3 Data')
-
-'''
-
-# Load Table 3_table
-
-'''
-
-Table_3_table_df = './Data/Table 3_table.csv'
-Table_3_table_data = pd.read_csv(Table_3_table_df)
-
-'''
-
-# Display data
-
-'''
-st.table(Table_3_table_data)
-
-'''
+if __name__ == "__main__":
+    chatbot
